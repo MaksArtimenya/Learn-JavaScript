f265a523925eea57165cd12f62ebff3114eabfeb
Maks Artimenya <maksimartimena@gmail.com>
2023-03-27 00:07:21 +0300

    Creating repository

---
+++ 2.11/ex3.js
@@ -0,0 +1,27 @@
+let login = prompt("Кто там?", "");
+
+if (login === "Админ")
+{
+    let password = prompt("Пароль?", "");
+
+    if (password === "Я Главный")
+    {
+        alert("Здравствуйте!");
+    }
+    else if (password === null || password === "")
+    {
+        alert("Отменено");
+    }
+    else
+    {
+        alert("Неверный пароль");
+    }
+}
+else if (login === null || login === "")
+{
+    alert("Отменено");
+}
+else
+{
+    alert("Я вас не знаю");
+}
